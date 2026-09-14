6f2b0ead8e0ae02270d9e897e70e0be861365ae4
kilyanbrs814-ctrl <kilyan.brs814@gmail.com>
2026-09-14 17:59:12 +0200

    Add mobile logo intro to site entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import MobileLogoIntro from "./components/MobileLogoIntro.jsx";
 import "./styles/global.css";
 import "./styles/components.css";
 
@@ -25,6 +26,7 @@ window.addEventListener("pageshow", () => {
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <MobileLogoIntro />
     <App />
   </StrictMode>
 );
